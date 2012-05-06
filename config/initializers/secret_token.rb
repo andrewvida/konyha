151cf807b47c0acc2e78c78ef75cc9f3a3d0dcdf
Andrew Vida <andrewjvida@gmail.com>
2012-05-06 15:19:54 -0400

    new again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Konyha::Application.config.secret_token = '1c873b812c7899a65f01a972114f96427e1d7edf37c844a7038c9d81d2381ccab6872e8e9f40de679384fbf25c261b6bdf614edaa9c696d24516ea82fca323d9'
